da5de95ce5d37c08375e6b96ccfdacb3aea2e3ae
legionary7931 <>
2021-12-16 13:03:25 +0000

    구현 연습

--- Implementation/4-2.py
+++ Implementation/4-2.py
@@ -1,2 +1,12 @@
 N = int(input())
 
+answer = 0
+
+for i in range(N+1):
+  for j in range(60):
+    for k in range(60):
+      if("3" in str(i) or "3" in str(j) or "3" in str(k)):
+        print("{} {} {}".format(i, j, k))
+        answer+=1
+
+print(answer)
