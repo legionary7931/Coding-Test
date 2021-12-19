5c6d0e045160fa5d777383ee32c65c90632e610f
legionary7931 <64356161+legionary7931@users.noreply.github.com>
2021-12-19 16:15:32 +0900

    Update 5-10.py

--- 이코테-BFSDFS/5-10.py
+++ 이코테-BFSDFS/5-10.py
@@ -1,1 +1,41 @@
+#149p 음료수 얼려 먹기
+#소요시간: 40분....
 
+from collections import deque
+
+def bfs(arr, row, col):
+  drow = [-1, 1, 0, 0] # 북 남 동 서
+  dcol = [0, 0, 1, -1]
+
+
+  queue = deque()
+  queue.append([row, col])
+  arr[row][col] = -1 # visit 처리
+
+  while(queue):
+    v = queue.popleft()
+
+    for i in range(4):
+      nrow = v[0]  + drow[i] #이거를 i대신 0으로 써서 헤맸다....
+      ncol = v[1] + dcol[i]
+
+      if(0<=nrow<M and 0<=ncol<N and [nrow, ncol] not in queue):
+        if(arr[nrow][ncol]==0):
+          arr[nrow][ncol] = - 1
+          queue.append([nrow, ncol])
+  return arr
+        
+N, M = map(int, input().split())
+arr = []
+
+for i in range(M):
+  arr.append(list(map(int, input())))
+
+ice_num = 0
+for i in range(M):
+  for j in range(N):
+    if(arr[i][j] == 0):
+      arr = bfs(arr, i, j)
+      ice_num+=1
+
+print(ice_num)
