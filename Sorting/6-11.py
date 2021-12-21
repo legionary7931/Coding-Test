f870427360ec6926393ba9c8bfbd02536f2ee02e
legionary7931 <>
2021-12-21 08:27:18 +0000

    coding_test_practice

--- Sorting/6-11.py
+++ Sorting/6-11.py
@@ -6,6 +6,8 @@ for i in range(N):
   arr.append((data[0], int(data[1])))
 
 arr = sorted(arr, key = lambda student: student[1])
+# 람다 함수 사용하는 법 숙지하기...
+
 
 for student in arr:
   print(student[0], end = " ")
